509c485d85df63033980e8100c31c156e78f7d9c
Stefan Progovac <stefan@quantalytics.ai>
2021-02-27 23:38:05 -0500

    [ADD] logic for searching

--- LoewsWeather/WeatherListViewController.swift
+++ LoewsWeather/WeatherListViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class WeatherListViewController: UITableViewController {
 
+	private let searchController = UISearchController(searchResultsController: nil)
+	private var cityName = ""
+
 	var forecasts: Forecasts? {
 		didSet {
 			DispatchQueue.main.async {
@@ -20,15 +23,15 @@ class WeatherListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-		sharedApi.getWeather(for: "Miami") { [unowned self] result in
-			switch result {
-			case let .success(forecast):
-				self.forecasts = forecast
-				print("This is the forecast: \(forecast)")
-			case let .failure(error):
-				print("This is the error: \(error)")
-			}
-		}
+		title = "Forecast"
+
+		searchController.searchResultsUpdater = self
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.searchBar.placeholder = "City Name"
+
+
+		navigationItem.searchController = searchController
+		definesPresentationContext = true
     }
 
 
@@ -54,49 +57,28 @@ class WeatherListViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
+}
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
+extension WeatherListViewController: UISearchResultsUpdating {
+	func updateSearchResults(for searchController: UISearchController) {
+		searchController.searchBar.delegate = self
+	}
+}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension WeatherListViewController: UISearchBarDelegate {
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		guard let cityName = searchBar.text, cityName.isEmpty == false else {
+			// TODO: return error
+			return
+		}
+		sharedApi.getWeather(for: cityName) { [unowned self] result in
+			switch result {
+			case let .success(forecast):
+				self.forecasts = forecast
+				print("This is the forecast: \(forecast)")
+			case let .failure(error):
+				print("This is the error: \(error)")
+			}
+		}
+	}
 }
