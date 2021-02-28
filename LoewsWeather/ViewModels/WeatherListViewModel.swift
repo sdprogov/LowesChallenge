140f01e0a89b718b69fc0b8bff55dc4c69e64712
Stefan Progovac <stefan@quantalytics.ai>
2021-02-28 01:00:39 -0500

    [ADD] loading

--- LoewsWeather/ViewModels/WeatherListViewModel.swift
+++ LoewsWeather/ViewModels/WeatherListViewModel.swift
@@ -29,6 +29,10 @@ class WeatherListViewModel: NSObject {
 	private weak var tableView: UITableView?
 	weak var delegate: WeatherListDelegate?
 
+	private let spinner = UIActivityIndicatorView()
+	private let loadingLabel = UILabel()
+	private let loadingView = UIView()
+
 	private var forecasts: Forecasts? {
 		didSet {
 			DispatchQueue.main.async {
@@ -39,16 +43,54 @@ class WeatherListViewModel: NSObject {
 
 
 	func findWeather(for city: String) {
+		setLoadingScreen()
 		sharedApi.getWeather(for: city) { [weak self] result in
+			DispatchQueue.main.async {
+				self?.removeLoadingScreen()
+			}
 			switch result {
 			case let .success(forecast):
-				self?.tableView?.restore()
 				self?.forecasts = forecast
 			case let .failure(error):
 				self?.tableView?.setEmptyMessage("No results found for search: \(city)")
 				self?.delegate?.showError(error: error.localizedDescription)
 			}
 		}
+	}
+
+	private func setLoadingScreen() {
+
+		guard let tableView = tableView else { return }
+
+		tableView.restore()
+
+		let width: CGFloat = 120
+		let height: CGFloat = 30
+		loadingView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+		loadingView.center = CGPoint(x: UIScreen.main.bounds.midX,y: UIScreen.main.bounds.midY - 44)
+
+		loadingLabel.textColor = .gray
+		loadingLabel.textAlignment = .center
+		loadingLabel.text = "Loading..."
+		loadingLabel.frame = CGRect(x: 0, y: 0, width: 140, height: 30)
+
+		spinner.style = UIActivityIndicatorView.Style.medium
+		spinner.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+		spinner.startAnimating()
+
+		loadingView.addSubview(spinner)
+		loadingView.addSubview(loadingLabel)
+
+		tableView.addSubview(loadingView)
+
+	}
+
+	private func removeLoadingScreen() {
+
+		spinner.stopAnimating()
+		spinner.isHidden = true
+		loadingLabel.isHidden = true
+
 	}
 }
 
